90bc9e52d7f4cdd57622fc29f1fabf0c94e6cdcc
Andrey Gneushev <andrey_gneushev@mail.ru>
2022-08-29 23:05:33 +0300

    ПР 13.2.3

--- app.js
+++ app.js
@@ -1,33 +1,38 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const userRouter = require('./routes/users');
+const cardRouter = require('./routes/cards');
+const { NOT_FOUND } = require('./utils/NotFound');
 
-const app = express();
 const { PORT = 3000 } = process.env;
+const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-mongoose.connect('mongodb://localhost:27017/mestodb',
-  {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-  });
+mongoose.connect('mongodb://localhost:27017/mestodb')
+  .then(() => console.log('connected'))
+  .catch((err) => console.log(`Ошибка ${err.name}: ${err.message}`));
 
-// Вставьте сюда _id созданного в предыдущем пункте пользователя
 app.use((req, res, next) => {
   req.user = {
     _id: '630a2fd6909cb351dc52dfc9',
   };
-
   next();
 });
 
-app.use('/', require('./routes/users'));
-app.use('/', require('./routes/cards'));
-app.use('*', require('./routes/NotFound'));
+app.use((req, res, next) => {
+  console.log(`${req.method}: ${req.path} ${JSON.stringify(req.body)}`);
+  next();
+});
+
+app.use('/', userRouter);
+app.use('/', cardRouter);
+app.use('/', (req, res) => {
+  res.status(NOT_FOUND).send({ message: 'Страница не найдена' });
+});
 
 app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}`);
+  console.log(`Example app listening on port ${PORT}`);
 });
