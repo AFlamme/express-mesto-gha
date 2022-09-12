3e42f12d9828cf41893df110aac3050895f60c9b
Andrey Gneushev <andrey_gneushev@mail.ru>
2022-09-12 13:12:40 +0300

    ПР 14. Корректировка app.js

--- app.js
+++ app.js
@@ -1,33 +1,59 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const ERRORS = require('./utils/utils');
+const { celebrate, Joi } = require('celebrate');
+const { errors } = require('celebrate');
+const { login, createUser } = require('./controllers/users');
+const auth = require('./middlewares/auth');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const { PORT = 3000 } = process.env;
 
 const app = express();
 
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}`);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
 });
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(requestLogger);
 
-app.use((req, res, next) => {
-  req.user = {
-    _id: '630a2fd6909cb351dc52dfc9',
-  };
+app.post('/signup', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(8),
+    name: Joi.string().required().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+    avatar: Joi.string()
+      .regex(/^(https?:\/\/)?([\da-z\\.-]+)\.([a-z\\.]{2,6})([\\/\w \\.-]*)*\/?$/),
+  }),
+}), createUser);
+app.post('/signin', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(8),
+  }),
+}), login);
 
+app.use(auth);
+
+app.use('/', require('./routes/users'));
+app.use('/', require('./routes/cards'));
+app.use('*', require('./routes/NotFound'));
+
+app.use(errorLogger);
+
+app.use(errors());
+app.use((err, req, res, next) => {
+  const { statusCode = 500, message } = err;
+  res.status(statusCode).send({ message: statusCode === 500 ? 'Ошибка по умолчанию' : message });
   next();
 });
 
-app.use('/users', require('./routes/users'));
-
-app.use('/cards', require('./routes/cards'));
-
-app.use('*', (req, res) => {
-  res.status(ERRORS.NOT_FOUND).send({ message: 'Страница не найдена' });
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
 });
-
-mongoose.connect('mongodb://localhost:27017/mestodb');
