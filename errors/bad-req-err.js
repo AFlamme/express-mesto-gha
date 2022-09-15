5041e3aa2b765020a8266ad6e9a2c4b38a683cae
Andrey Gneushev <andrey_gneushev@mail.ru>
2022-09-15 13:01:11 +0300

    ПР 14.2. Исправления после второй проверки

--- errors/bad-req-err.js
+++ errors/bad-req-err.js
@@ -1,9 +1,9 @@
 class BadReqError extends Error {
-    constructor(message) {
-      super(message);
-      this.statusCode = 400;
-    }
+  constructor(message) {
+    super(message);
+    this.statusCode = 400;
   }
-  
-  module.exports = BadReqError;
+}
+
+module.exports = BadReqError;
   
